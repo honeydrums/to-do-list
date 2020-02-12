0f70e75a218f1d9b7ec0ea81abc483e1faf6ebcb
Vyacheslav Nikolaev <honeydrums@icloud.com>
2020-02-12 21:26:31 +0300

    add script...
    "delete" button with icon, some styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-// 
+function onPageLoaded() {
+	const input = document.querySelector("input[type='text']");
+	const ul = document.querySelector("ul.todo__list");
+
+	// функция создания пункта списка
+	function createToDo() {
+		const li = document.createElement("li");
+		const checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		const span = document.createElement("span");
+		const deleteButton = document.createElement("button");
+		li.classList.add("todo__list-item", "todo__list-item_position");
+		checkbox.classList.add("todo__list-checkbox");
+		deleteButton.classList.add("todo__delete");
+		const newToDo = input.value;
+		span.append(newToDo);
+
+		ul.appendChild(li).append(checkbox);
+		ul.appendChild(li).append(span);
+		ul.appendChild(li).append(deleteButton);
+		input.value = "";
+	}
+
+	// функция удаления пункта списка
+
+	//вызов функции, создающей пункт списка
+	input.addEventListener("keypress", (keyPressed) => {
+		const keyEnter = 13;
+		if (keyPressed.which == keyEnter) {
+			createToDo();
+		}
+	});
+	// вызов функции, удаляющей пункт списка
+	
+	// ul.addEventListener("click", onClickTodo);
+}
+
+document.addEventListener("DOMContentLoaded", onPageLoaded);
